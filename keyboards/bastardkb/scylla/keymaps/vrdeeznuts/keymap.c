fa38651ca50636bac2836f21b5d911d9f790796d
yuki.adams <yadams@appliedres.com>
2025-03-04 12:23:09 -0500

    fixed missing key

--- keyboards/bastardkb/scylla/keymaps/vrdeeznuts/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/vrdeeznuts/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              //ROW 3//
                              //KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,//
                              // HOME ROW MODS GACS
-                             GUI_A, ALT_S, CTL_D, SFT_F, KC_G, KC_H, SFT_J, CTL_K, ALT_L, GUI_SCLN, KC_QUOT,
+                             KC_LSFT, GUI_A, ALT_S, CTL_D, SFT_F, KC_G, KC_H, SFT_J, CTL_K, ALT_L, GUI_SCLN, KC_QUOT,
                              //-------------------------------------------------//-----------------------------------------------------------//
                              //ROW 4//
                              KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
